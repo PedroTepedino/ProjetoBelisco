e6c209b9595dba448882990317d471f8cbfcee2a
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-03-19 03:30:35 -0300

    Starts the Tool Passei uma raiva com erro idiota kkkkk. PS: cuidado com foreach dentro de foreach

---
+++ ProjetoBelisco/Assets/Scripts/Tools/Editor/SceneEssentials.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using System.Linq;
+
+
+public class SceneEssentials : OdinMenuEditorWindow
+{
+    private CreateNewSceneEssentials _createNewSceneEssentials;
+
+    private List<GameObject> _missingGameObjects;
+
+    [MenuItem("Tools/Scene Essentials", priority = -10000)]
+    private static void OpenWindow()
+    {
+        GetWindow<SceneEssentials>().Show();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (_createNewSceneEssentials != null)
+        {
+            DestroyImmediate(_createNewSceneEssentials.EssentialObjects);
+        }
+    }
+
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        var tree = new OdinMenuTree();
+
+        _createNewSceneEssentials = new CreateNewSceneEssentials();
+
+        tree.Add("Create New Scene Type", _createNewSceneEssentials);
+        tree.AddAllAssetsAtPath("Scene Essentials", "Assets/Resources/Tools/SceneEssentials", typeof(SceneEssentialObjects));
+        
+        return tree;
+    }
+    
+
+    protected override void OnBeginDrawEditors()
+    {   
+        OdinMenuTreeSelection selected = this.MenuTree.Selection;
+
+        if (selected.SelectedValue != null)
+        {
+            if (selected.SelectedValue.GetType() == typeof(SceneEssentialObjects))
+            {
+                SirenixEditorGUI.BeginHorizontalToolbar();
+                {
+                    GUILayout.FlexibleSpace();
+                    if (SirenixEditorGUI.ToolbarButton("Delete Current"))
+                    {
+                        SceneEssentialObjects asset = selected.SelectedValue as SceneEssentialObjects;
+                        string path = AssetDatabase.GetAssetPath(asset);
+                        AssetDatabase.DeleteAsset(path);
+                        AssetDatabase.SaveAssets();
+                    }
+                }
+                SirenixEditorGUI.EndHorizontalToolbar();
+            }
+        }
+    }
+
+    protected override void OnEndDrawEditors()
+    {
+        Debug.Log(_missingGameObjects);
+        OdinMenuTreeSelection selected = this.MenuTree.Selection;
+
+        if (selected.SelectedValue != null)
+        {
+            if (selected.SelectedValue.GetType() == typeof(SceneEssentialObjects))
+            {
+                if (_missingGameObjects != null)
+                {
+                    foreach (GameObject obj in _missingGameObjects)
+                    {
+                        SirenixEditorGUI.WarningMessageBox("Missing -" + obj.name + "- Prefab.");
+                    }
+                }
+
+                GUILayout.FlexibleSpace();
+                SirenixEditorGUI.BeginHorizontalToolbar();
+                {
+                    GUILayout.FlexibleSpace();
+                    if (_missingGameObjects !=  null)
+                    {
+                        if (SirenixEditorGUI.ToolbarButton("Add Missing Objects"))
+                        {
+                        }
+                    }
+                    if (SirenixEditorGUI.ToolbarButton("Check Scene"))
+                    {
+                        SceneEssentialObjects asset = selected.SelectedValue as SceneEssentialObjects;
+                        CheckScene(asset);
+                    }
+                }
+                SirenixEditorGUI.EndHorizontalToolbar();
+            }
+        }
+    }
+
+    private void CheckScene(SceneEssentialObjects asset)
+    {
+        GameObject[] objectsInScene = Object.FindObjectsOfType<GameObject>();
+
+        if (_missingGameObjects != null)
+        {
+            _missingGameObjects = null;
+        }
+
+        _missingGameObjects = new List<GameObject>();
+
+        foreach (GameObject assetObj in asset.Prefabs)
+        {
+            bool objectExists = false;
+            foreach (GameObject sceneObj in objectsInScene)
+            {
+                if (PrefabUtility.GetCorrespondingObjectFromSource(sceneObj) == assetObj)
+                {
+                    objectExists = true;
+                }
+            }            
+
+            if (!objectExists)
+            {
+                _missingGameObjects.Add(assetObj);
+            }
+        }
+
+        if (_missingGameObjects.Count == 0)
+        {
+            _missingGameObjects = null;
+        }
+    }
+
+    public class CreateNewSceneEssentials
+    {
+        public CreateNewSceneEssentials()
+        {
+            EssentialObjects = ScriptableObject.CreateInstance<SceneEssentialObjects>();
+            EssentialObjects.Prefabs = new List<GameObject>();
+            EssentialsName = null;
+        }
+
+        [LabelText("Scene Type: ")]
+        [SuffixLabel("Name", overlay: true)]
+        [Required("Name for the Scene Type is Required!")]
+        [ValidateInput("ValidateName", "Must Have a unique name.", InfoMessageType.Error)]
+        public string EssentialsName;
+
+        [LabelText("Essentials")]
+        [Tooltip("The Objectes that are essetial to this type os scene")]
+        [InlineEditor(ObjectFieldMode = InlineEditorObjectFieldModes.Hidden)]
+        public SceneEssentialObjects EssentialObjects;
+
+        [Button("Save")]
+        private void CreateNewSceneType()
+        {
+            AssetDatabase.CreateAsset(EssentialObjects, "Assets/Resources/Tools/SceneEssentials/" + EssentialsName + ".asset");
+            AssetDatabase.SaveAssets();
+
+            // Create new Instance of the Scriptable Object
+            EssentialObjects = ScriptableObject.CreateInstance<SceneEssentialObjects>();
+            EssentialObjects.Prefabs = new List<GameObject>();
+            EssentialsName = null;
+        }
+
+        private bool ValidateName(string name)
+        {
+            if (name == null)
+            {
+                return false;
+            }
+            else
+            {
+                SceneEssentialObjects[] sceneTypes = Resources.LoadAll<SceneEssentialObjects>("Tools/SceneEssentials");
+
+                foreach (SceneEssentialObjects st in sceneTypes)
+                {
+                    if (name.ToLower() == st.name.ToLower())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ProjetoBelisco/Assets/Scripts/Tools/SceneEssentialObjects.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "SceneEssentials", menuName = "Tools/SceneEssentials", order = 1)]
+public class SceneEssentialObjects : ScriptableObject
+{
+    [AssetsOnly]
+    public List<GameObject> Prefabs;
+}
