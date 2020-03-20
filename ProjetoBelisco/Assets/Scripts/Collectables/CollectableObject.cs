940f8247e1ff0d675e8b1b328998e7d408facfb6
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-03-19 22:31:38 -0300

    Collectables Begin

---
+++ ProjetoBelisco/Assets/Scripts/Collectables/CollectableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CollectableObject : MonoBehaviour
+{
+    [SerializeField] private CollectableParameters _parameters;
+
+
+}
---
+++ ProjetoBelisco/Assets/Scripts/Collectables/Effects/BaseColletableEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class BaseColletableEffect : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        
+    }
+
+    protected virtual void OnDestroy()
+    {
+
+    }
+
+    public abstract void Effect();
+}
