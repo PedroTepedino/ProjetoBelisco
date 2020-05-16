97af2e3ccebf20f10bade531998ef8043e712087
DCarrijo <davi.carrijo1@gmail.com>
2020-05-15 21:13:30 -0300

    VFXKRAI

---
+++ ProjetoBelisco/Assets/Scripts/Player/PlayParticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleSystem;
+
+
+
+    private void Awake()
+    {
+        PlayerGrounder.OnTouchGround += EmitParticle;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerGrounder.OnTouchGround -= EmitParticle;
+    }
+
+    public void EmitParticle()
+    {
+        _particleSystem.Play();
+    }
+}
