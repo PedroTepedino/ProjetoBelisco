1e36b9c5983eaee7c432d6c0a4246c72091e53ba
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-05-23 06:04:06 -0300

    Fecha dev

---
+++ ProjetoBelisco/Assets/Scripts/Player/PlayerDamageParticle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerDamageParticle : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _damageParticles;
+
+    private void Awake()
+    {
+        PlayerLife.OnPlayerDamage += PlayParticle;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerLife.OnPlayerDamage -= PlayParticle;
+    }
+
+    private void PlayParticle(int damage, int totalHealth)
+    {
+        _damageParticles.Play();
+    }
+}
