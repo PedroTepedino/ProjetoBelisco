10e0b4394748454d90d3bc55345a6a173cbd9a5c
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-06-16 18:19:09 -0300

    Adicionei icones em algumas pastas

---
+++ ProjetoBelisco/Assets/Scripts/Player/NewPlayer/PlayerParameters.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Player.NewPlayer
+{
+    [CreateAssetMenu(menuName = "Player Parameters")]
+    public class PlayerParameters : ScriptableObject
+    {
+        [BoxGroup("Movement")]
+        
+        [SerializeField] private float _movementSpeed = 5f;
+        public float MovementSpeed => _movementSpeed;
+    }
+}
