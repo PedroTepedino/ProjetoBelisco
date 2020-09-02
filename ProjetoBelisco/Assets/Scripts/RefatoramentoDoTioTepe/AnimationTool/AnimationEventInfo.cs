60935bb46044fea01c9222c807f771eac1724ef5
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-09-01 21:19:26 -0300

    Ebaaaaa Branch errada

---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/AnimationTool/AnimationEventInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class AnimationEventInfo
+{
+    public int ClipIndex;
+    public float Time;
+    public string FunctionName;
+}
---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/AnimationTool/Testezera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Testezera : MonoBehaviour
+{
+    public void TestezeraDavizera()
+    {
+        Debug.Log("KARAI");
+    }
+}
