986dd785aa6658589283360c2b3ca8ff3f38ec4e
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-09-07 19:13:13 -0300

    Spikes

---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/UI/Spikes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RefatoramentoDoTioTepe
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Spikes : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            other.GetComponent<IHittable>()?.Hit(10000);
+        }
+    }
+}
