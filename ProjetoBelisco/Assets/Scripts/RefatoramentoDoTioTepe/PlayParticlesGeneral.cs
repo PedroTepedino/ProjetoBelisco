ed50797fe42ce6f0569e60ac5e31fab559c03eda
Davi Carrijo <davi.carrijo1@gmail.com>
2020-09-07 00:21:42 -0300

    inicio mapa 2

---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/PlayParticlesGeneral.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RefatoramentoDoTioTepe
+{
+    public class PlayParticlesGeneral : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _particleSystem;
+
+
+        
+        public void EmitParticle()
+        {
+            _particleSystem.Play();
+        }
+    }
+}
