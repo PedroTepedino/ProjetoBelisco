064ee24997348662aa81654da24144f6be87bdf3
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-10-18 03:51:10 -0300

    Disparador de flechas pronto

---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/Traps/Arrow.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using GameScripts.PoolingSystem;
+using UnityEngine;
+
+namespace RefatoramentoDoTioTepe
+{
+    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Arrow : MonoBehaviour, IPooledObject
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private float _velocity;
+        [SerializeField] private int _damage = 1;
+
+        private Tween _animation;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var hittable = other.GetComponent<IHittable>();
+            if (hittable != null)
+            {
+                hittable.Hit(_damage);
+            }
+            
+            _rigidbody.velocity = Vector2.zero;
+            
+            _animation.SmoothRewind();
+        }
+        
+        
+        public void OnObjectSpawn()
+        {
+            _animation = this.transform.DOScale(1f, 0.2f).From(0f).SetAutoKill(false).SetEase(Ease.InOutBack);
+            _animation.onRewind += () => _animation.Kill();
+            _animation.onRewind += () => this.gameObject.SetActive(false);
+            _rigidbody.velocity = this.transform.up * _velocity;
+        }
+
+        private void OnValidate()
+        {
+            this.GetComponent<Collider2D>().isTrigger = true;
+            if (_rigidbody == null)
+            {
+                _rigidbody = this.GetComponent<Rigidbody2D>();
+            }
+        }
+    }
+}
---
+++ ProjetoBelisco/Assets/Scripts/RefatoramentoDoTioTepe/Traps/ArrowShooter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using GameScripts.PoolingSystem;
+using Rewired;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace RefatoramentoDoTioTepe
+{
+    public class ArrowShooter : MonoBehaviour
+    {
+        [SerializeField] [EnumToggleButtons] private Directions _shootingDirectionEnum = Directions.Right;
+        private Vector2 _shootingDirection;
+
+        [SerializeField] private LayerMask _layersToCheck;
+        [SerializeField] private float _colliderRadius = 1f;
+        [SerializeField] private float _maxTravelDistance = 10f;
+        
+        private RaycastHit2D[] _colliders = new RaycastHit2D[5];
+
+        [SerializeField] private float _timeBetweenShots = 1f;
+        private float _timer = 0f;
+
+        private void Awake()
+        {
+            _shootingDirection = HelperFunctions.GetDirectionVector(_shootingDirectionEnum);
+        }
+
+        private void Update()
+        {
+            if (_timer <= 0)
+            {
+                int collisions = Physics2D.CircleCastNonAlloc(this.transform.position, _colliderRadius, _shootingDirection, _colliders, _maxTravelDistance, _layersToCheck);
+                for (int i = 0; i < collisions; i++)
+                {
+                    if (_colliders[i].collider.gameObject.CompareTag("Player"))
+                    {
+                        Shoot();
+                    }
+                } 
+            }
+            else
+            {
+                _timer -= Time.deltaTime;
+            }
+        }
+
+        private void Shoot()
+        {
+            _timer = _timeBetweenShots;
+            Pooler.Instance.SpawnFromPool("Arrow", this.transform.position,
+                HelperFunctions.GetDirectionAngle(_shootingDirectionEnum));
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(this.transform.position + (Vector3)(HelperFunctions.GetDirectionVector(_shootingDirectionEnum) * _maxTravelDistance) , _colliderRadius);
+            Gizmos.DrawLine(this.transform.position, this.transform.position + (Vector3)(HelperFunctions.GetDirectionVector(_shootingDirectionEnum) * _maxTravelDistance));
+        }
+    }
+}
